e66555aad0e89b0a19e358d3106430e6049aabe3
Toranian <mediamanmorrow@gmail.com>
2017-11-10 07:05:31 -0800

    Changed Var Names
    Changed some variable names so they are easier to type and understand.

--- Main.py
+++ Main.py
@@ -27,25 +27,23 @@ def random_rgb_colour():
 # Game loop
 def game_loop():
 
-    paddle_speed = 5
+    paddle_speed = 6
     speed = 4
     game_exit = False
-    lead_x = WIDTH / 2
+    ball_x = WIDTH / 2
     lead_y = HEIGHT / 2
-
     ball_size = 15
 
     # Paddles
-    paddle_1_x = 10
-    paddle_1_y = (WIDTH / 2) - 150
+    paddle_x = 10
+    paddle_y = (WIDTH / 2) - 150
     paddle_2_x = WIDTH - 20
     paddle_2_y = (WIDTH / 2) - 150
     paddle_size = [10, 100]
 
-    lead_x_change = -speed
-    lead_y_change = speed
+    ball_x_change = -speed
+    ball_y_change = speed
 
-    lead_paddle_x = 0
     lead_paddle_y = 0
 
     while not game_exit:
@@ -62,29 +60,37 @@ def game_loop():
                 if event.key == pygame.K_s:
                     lead_paddle_y = paddle_speed
 
-        # Bounce
-        if lead_x <= paddle_1_x:
-            lead_x_change = -speed
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    lead_paddle_y = 0
+
+                if event.key == pygame.K_s:
+                    lead_paddle_y = 0
+
+        # Paddle collision
+
+        if ball_x <= paddle_y and paddle_x:
+            ball_x_change = -speed
+            print('success')
 
         # Game Over
-        if lead_x >= WIDTH:
-            lead_x_change = -speed
-            # game_exit = True
-        elif lead_x <= 0:
-            lead_x_change = speed
-            # game_exit = True
+        if ball_x > WIDTH:
+            # ball_x_change = -speed
+            return "hey"
+        elif ball_x < 0:
+            ball_x_change = speed
         if lead_y >= HEIGHT:
-            lead_y_change = -speed
+            ball_y_change = -speed
         elif lead_y <= 0:
-            lead_y_change = speed
+            ball_y_change = speed
 
-        lead_x += lead_x_change
-        lead_y += lead_y_change
-        paddle_1_y += lead_paddle_y
+        ball_x += ball_x_change
+        lead_y += ball_y_change
+        paddle_y += lead_paddle_y
 
         gameDisplay.fill(BLACK)
-        pygame.draw.rect(gameDisplay, WHITE, [lead_x, lead_y, ball_size, ball_size])
-        pygame.draw.rect(gameDisplay, WHITE, [paddle_1_x, paddle_1_y, paddle_size[0], paddle_size[1]])
+        pygame.draw.rect(gameDisplay, WHITE, [ball_x, lead_y, ball_size, ball_size])
+        pygame.draw.rect(gameDisplay, WHITE, [paddle_x, paddle_y, paddle_size[0], paddle_size[1]])
         pygame.draw.rect(gameDisplay, WHITE, [paddle_2_x, lead_y - 50, paddle_size[0], paddle_size[1]])
         pygame.display.update()
 
